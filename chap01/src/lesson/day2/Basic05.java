491d6d761669314c8d0ad1c3c7185b78cde13605
greenrok <kdr0112@gmail.com>
2022-07-12 15:16:22 +0900

    Java commit practice again

---
+++ chap01/src/lesson/day2/Basic05.java
@@ -0,0 +1,25 @@
+package lesson.day2;
+
+public class Basic05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float splitNumber1 = 1234/1.0f;
+		double splitNumber2 = 1234/1.0;
+		float splitNumber3 = (float)(1234/1.0);
+		
+		System.out.println(splitNumber1);
+		System.out.println(splitNumber2);
+		System.out.println(splitNumber3);
+		
+		int x = 1;
+		int y = 1;
+		
+		System.out.println("++x --> " + ++x);
+		System.out.println("y++ --> " + y++);
+		
+		System.out.println("x --> " + x);
+		System.out.println("y --> " + y);
+	}
+
+}
